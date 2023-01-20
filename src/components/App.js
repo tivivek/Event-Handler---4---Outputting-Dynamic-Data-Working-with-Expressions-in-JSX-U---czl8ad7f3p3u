70709202d8d46bd84d66e8199800b4c2df20ac30
Vivek Tiwari <79350756+tivivek@users.noreply.github.com>
2023-01-20 14:01:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import '../styles/App.css';
 const App = () => {
 
   const handleDoubleClick = (event) =>{
-  
+    if (event.detail === 2) {
+      console.log('I was double clicked');
+    } else {
+      console.log('I was not double clicked')
+    }
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
